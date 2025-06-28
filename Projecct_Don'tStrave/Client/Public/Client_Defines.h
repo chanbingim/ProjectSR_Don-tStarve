83345e64c5287b2aa72819ce4345bae09ae05a70
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2025-06-28 21:58:43 +0900

    ADD Define
    아이템 데이터 구조체 추가

--- Projecct_Don'tStrave/Client/Public/Client_Defines.h
+++ Projecct_Don'tStrave/Client/Public/Client_Defines.h
@@ -17,7 +17,7 @@ namespace Client
 	const unsigned int			g_iWinSizeY = 720;
 
 	enum class LEVEL { STATIC, GAMEPLAY_STATIC, LOADING, LOGO, TUTORIAL, GAMEPLAY, END };
-	enum class ITEM_TYPE { MERTARIAL, FOOD, EQUIPMENT };
+	enum class ITEM_TYPE { MERTARIAL, FOOD, EQUIPMENT, STRUCTURE};
 	enum class SLOT { NORMAL, HAND, HAT, ARMOR, INFO };
 	enum class CATEGORY { TOOL, FIRE, MACHINE, WEAPON, FOOD, STRUCTURE, END };
 
@@ -30,6 +30,23 @@ namespace Client
 		float			fDurability{};
 		D3DXVECTOR3		vPosition{};
 	}ITEM_DESC;
+
+	typedef struct Item_Data
+	{
+		unsigned int	iItemID{};
+		string			strName{};
+		ITEM_TYPE		eItemType{};
+		string			strKorName{};
+		SLOT			eSlot{};
+		string			strExplanation = {};
+		unsigned int    iMaterial_Count{};
+		unsigned int	iMaterialID_1{};
+		unsigned int	iNumMaterial_1{};
+		unsigned int	iMaterialID_2{};
+		unsigned int	iNumMaterial_2{};
+		unsigned int	iMaterialID_3{};
+		unsigned int	iNumMaterial_3{};
+	}ITEM_DATA;
 }
 
 using namespace Client;
