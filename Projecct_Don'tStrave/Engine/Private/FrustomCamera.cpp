3a713aecf6954dc440d00ec3ea50347db24aa52c
chanbingim <127704542+chanbingim@users.noreply.github.com>
2025-07-03 12:26:46 +0900

    Merge_Kjh
    1. 아침 김정환 병합
    2. 프로스텀 컬링 다시 적용

--- Projecct_Don'tStrave/Engine/Private/FrustomCamera.cpp
+++ Projecct_Don'tStrave/Engine/Private/FrustomCamera.cpp
@@ -65,6 +65,7 @@ void CFrustomCamera::Late_Update(_float fTimeDelta)
 
 HRESULT CFrustomCamera::Render()
 {
+
 	return S_OK;
 }
 
@@ -75,18 +76,18 @@ void CFrustomCamera::Compute_CameraMatrix()
 	m_pGraphic_Device->SetTransform(D3DTS_VIEW, &m_pTransformCom->Get_InverseWorldMat());
 	m_pGraphic_Device->SetTransform(D3DTS_PROJECTION, D3DXMatrixPerspectiveFovLH(&m_ProjMatrix, m_fFov, m_fAspect, m_fNear, m_fFar));
 
-	for (auto iter : m_FustomPoints)
+	for (auto& iter : m_FustomPoints)
 	{
-		_float3	Point = {};
 		_matrix	InvPorjection = {};
 		D3DXMatrixInverse(&InvPorjection, NULL, &m_ProjMatrix);
 
+		InvPorjection = InvPorjection * m_pTransformCom->Get_World() ;
+		_float3	Point = {};
 		//투영 행렬의 역행렬을 곱해 view 스페이스 상의 좌표를 얻어낸다.
-		D3DXVec3TransformCoord(&Point, &Point, &InvPorjection);
-
+		D3DXVec3TransformCoord(&Point, &iter, &InvPorjection);
+		
 		//뷰스페이스의 역행렬을 곱해서 월드공간상의 좌표를 얻어낸다.
 		//즉 월드 좌표를 곱해서 월드 공간상의 좌표를 얻어낸다.
-		D3DXVec3TransformCoord(&Point, &Point, &m_pTransformCom->Get_World());
 		m_ComputeFustomPoints.push_back(Point);
 	}
 }
@@ -102,12 +103,12 @@ _bool CFrustomCamera::IsInObject(const _float3& Point, _float Offset)
 	//6개의 평면의 방정식에서 모두 0 >= 값이 나오면 랜더링
 
 	D3DXPlaneFromPoints(&m_FustomPlane[0], &m_ComputeFustomPoints[0], &m_ComputeFustomPoints[1], &m_ComputeFustomPoints[2]);
-	D3DXPlaneFromPoints(&m_FustomPlane[1], &m_ComputeFustomPoints[0], &m_ComputeFustomPoints[4], &m_ComputeFustomPoints[5]);
-	D3DXPlaneFromPoints(&m_FustomPlane[2], &m_ComputeFustomPoints[0], &m_ComputeFustomPoints[3], &m_ComputeFustomPoints[7]);
+	D3DXPlaneFromPoints(&m_FustomPlane[1], &m_ComputeFustomPoints[1], &m_ComputeFustomPoints[5], &m_ComputeFustomPoints[6]);
+	D3DXPlaneFromPoints(&m_FustomPlane[2], &m_ComputeFustomPoints[3], &m_ComputeFustomPoints[2], &m_ComputeFustomPoints[6]);
 
-	D3DXPlaneFromPoints(&m_FustomPlane[3], &m_ComputeFustomPoints[6], &m_ComputeFustomPoints[2], &m_ComputeFustomPoints[1]);
-	D3DXPlaneFromPoints(&m_FustomPlane[4], &m_ComputeFustomPoints[6], &m_ComputeFustomPoints[2], &m_ComputeFustomPoints[3]);
-	D3DXPlaneFromPoints(&m_FustomPlane[5], &m_ComputeFustomPoints[6], &m_ComputeFustomPoints[7], &m_ComputeFustomPoints[4]);
+	D3DXPlaneFromPoints(&m_FustomPlane[3], &m_ComputeFustomPoints[4], &m_ComputeFustomPoints[0], &m_ComputeFustomPoints[3]);
+	D3DXPlaneFromPoints(&m_FustomPlane[4], &m_ComputeFustomPoints[5], &m_ComputeFustomPoints[4], &m_ComputeFustomPoints[7]);
+	D3DXPlaneFromPoints(&m_FustomPlane[5], &m_ComputeFustomPoints[4], &m_ComputeFustomPoints[5], &m_ComputeFustomPoints[1]);
 
 	for (auto& iter : m_FustomPlane)
 	{
